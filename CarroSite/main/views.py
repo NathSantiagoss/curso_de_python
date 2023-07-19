3345d854f76f2ea813b6d96708f67798683a73ba
Nathalia Santiago <138415060+NathaliaSantiagoss@users.noreply.github.com>
2023-07-19 11:22:34 -0300

    19/07/2023

--- CarroSite/main/views.py
+++ CarroSite/main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Carro
 
-# Create your views here.
+def criar_carro(request):
+    if request.method == 'POST':
+        modelo = request.POST.get('modelo')
+        marca = request.POST.get('marca')
+        cor = request.POST.get('cor')
+        data_compra = request.POST.get('data_compra')
+        observacoes = request.POST.get('observacoes')
+
+        print(modelo)
+
+        carro = Carro(modelo=modelo, marca=marca, 
+                      cor=cor, 
+                      data_compra=data_compra, 
+                      observacoes=observacoes
+                      )
+        carro.save()
+
+        return redirect('formulario_carro')
+    else:
+        return render(request, 'main/index.html')
